019910f82bd57a84262619ee0b59910f09d3f93f
Prince Kumar <princekumar201926@gmail.com>
2025-02-09 16:28:39 +0530

    Fixed bug for tailwind

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="./assets/css/output.css" rel="stylesheet">
+
+</head>
+<body>
+    <p class=" text-5xl/loose font-semibold underline-offset-2 text-rose-500 underline text-center pt-13 selection:bg-sky-300 tracking-wider ">Testing Tailwind CSS. <br> Try using Tailwind.</p>
+</body>
+</html>
+
+
