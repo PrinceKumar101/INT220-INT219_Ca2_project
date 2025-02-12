5de8685024f51fa23d3543d31a1892797f3798ac
Prince Kumar <princekumar201926@gmail.com>
2025-02-12 19:56:48 +0530

    final setup

--- public/index.php
+++ public/index.php
@@ -1,15 +1,19 @@
 <?php
-// $content = isset($_GET['page']) ? $_GET['page'] . '.php' : 'home.php';
-// if (!file_exists($content)) {
-//     $content = '404.php'; 
-// }
 
-$allowed_pages = ['home', 'about', 'contact','login','dashboard']; // Add valid page names
+require_once '../vendor/autoload.php';
+use Dotenv\Dotenv;
+$dotenv = Dotenv::createImmutable(__DIR__.'/../');
+$dotenv->load();
+
+
+$allowed_pages = ['home', 'about', 'contact','login','dashboard'];
 $page = isset($_GET['page']) && in_array($_GET['page'], $allowed_pages) ? $_GET['page'] : 'home';
 $content = "./$page.php";
 if (!file_exists($content)) {
     $content = '404.php';
 }
+
+$name = $_ENV['NAME'];
 
 ?>
 
