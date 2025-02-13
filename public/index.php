1df71f1bbd6be640d7c1f3f2b04baa1929eeace5
Prince Kumar <princekumar201926@gmail.com>
2025-02-13 11:24:51 +0530

    basic setup and usage fo header file

--- public/index.php
+++ public/index.php
@@ -1,17 +1,11 @@
 <?php
 
-require_once '../vendor/autoload.php';
-use Dotenv\Dotenv;
-$dotenv = Dotenv::createImmutable(__DIR__.'/../');
-$dotenv->load();
 
 
-$allowed_pages = ['home', 'about', 'contact','login','dashboard'];
-$page = isset($_GET['page']) && in_array($_GET['page'], $allowed_pages) ? $_GET['page'] : 'home';
-$content = "./$page.php";
-if (!file_exists($content)) {
-    $content = '404.php';
-}
+require_once '../backend/includes/header.php';
+
+
+
 
 $name = $_ENV['NAME'];
 
